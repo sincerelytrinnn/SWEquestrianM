c690b0a385e25a0753d7c6a8a2c2affec6245fbd
Jahleel A <jahleelabraham@hotmail.com>
2020-10-08 18:54:07 +0100

    Wee

---
+++ src/main/java/com/alaharranhonor/swem/blocks/RegularBlockBase.java
@@ -0,0 +1,9 @@
+package com.alaharranhonor.swem.blocks;
+
+import net.minecraft.block.Block;
+
+public class RegularBlockBase extends Block {
+    public RegularBlockBase(Properties properties) {
+        super(properties);
+    }
+}
