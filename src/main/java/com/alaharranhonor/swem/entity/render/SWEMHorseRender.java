868c4eb39aecb16536302a2a7b61ec6b5e2ab876
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-13 10:59:07 +0200

    Update the textures and make the model animated

--- src/main/java/com/alaharranhonor/swem/entity/render/SWEMHorseRender.java
+++ src/main/java/com/alaharranhonor/swem/entity/render/SWEMHorseRender.java
@@ -2,19 +2,18 @@ package com.alaharranhonor.swem.entity.render;
 
 import com.alaharranhonor.swem.SWEM;
 import com.alaharranhonor.swem.entities.SWEMHorseEntity;
-import com.alaharranhonor.swem.entities.SWEMHorseEntity_Backup;
 import com.alaharranhonor.swem.entity.model.SWEMHorseModel;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.util.ResourceLocation;
 
-public class SWEMHorseRender extends MobRenderer<SWEMHorseEntity, SWEMHorseModel<SWEMHorseEntity>> {
-
+public class SWEMHorseRender extends MobRenderer<SWEMHorseEntity, SWEMHorseModel> {
     protected static final ResourceLocation TEXTURE = new ResourceLocation(SWEM.MOD_ID, "textures/entity/swem_horse.png");
 
     public SWEMHorseRender(EntityRendererManager renderManagerIn) {
-        super(renderManagerIn, new SWEMHorseModel<>(), 1.5f);
+        super(renderManagerIn, new SWEMHorseModel(), 1.5f);
     }
+
 
     @Override
     public ResourceLocation getEntityTexture(SWEMHorseEntity entity) {
