863287c1df9227749a03958f32e588aa688c3ff7
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-13 10:59:40 +0200

    added base file for natural spawning entities

--- src/main/java/com/alaharranhonor/swem/world/gen/SWEMEntitySpawns.java
+++ src/main/java/com/alaharranhonor/swem/world/gen/SWEMEntitySpawns.java
@@ -1,2 +1,37 @@
-package com.alaharranhonor.swem.world.gen;public class SWEMEntitySpawns {
+package com.alaharranhonor.swem.world.gen;
+
+import com.alaharranhonor.swem.SWEM;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod.EventBusSubscriber(modid = SWEM.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class SWEMEntitySpawns {
+
+	@SubscribeEvent
+	public static void spawnEntities(FMLLoadCompleteEvent event)
+	{
+		for(Biome biome : ForgeRegistries.BIOMES)
+		{
+			if (biome.getCategory() == Biome.Category.NETHER)
+			{
+				// Nether entities
+			}
+			else if (biome.getCategory() == Biome.Category.THEEND)
+			{
+				// End entities?
+			}
+			else
+			{
+				if (biome.getCategory() != Biome.Category.OCEAN)
+				{
+					// Make sure the biome is not an occean.
+					// Hmmm not sure.
+				}
+				// Overworld entities
+			}
+		}
+	}
 }
