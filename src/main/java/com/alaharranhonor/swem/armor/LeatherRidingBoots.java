9f3093c0a661e7b167d4c135bec22b91a646554b
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-13 12:21:20 +0200

    Make the leather riding boots, an armor-item with the encahnt of upstep, when crafted.

--- src/main/java/com/alaharranhonor/swem/armor/LeatherRidingBoots.java
+++ src/main/java/com/alaharranhonor/swem/armor/LeatherRidingBoots.java
@@ -1,2 +1,34 @@
-package com.alaharranhonor.swem.armor;public class LeatherRidingBoots {
+package com.alaharranhonor.swem.armor;
+
+import com.alaharranhonor.swem.util.RegistryHandler;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class LeatherRidingBoots extends ArmorItem {
+	public LeatherRidingBoots(IArmorMaterial materialIn, EquipmentSlotType slot, Properties builderIn) {
+		super(materialIn, slot, builderIn);
+	}
+
+	/**
+	 * Called when item is crafted/smelted. Used only by maps so far.
+	 *
+	 * @param stack
+	 * @param worldIn
+	 * @param playerIn
+	 */
+	@Override
+	public void onCreated(ItemStack stack, World worldIn, PlayerEntity playerIn) {
+		super.onCreated(stack, worldIn, playerIn);
+		stack.addEnchantment(RegistryHandler.UPSTEP.get(), 1);
+	}
+
+	@Override
+	public boolean hasEffect(ItemStack stack) {
+		super.hasEffect(stack);
+		return true;
+	}
 }
