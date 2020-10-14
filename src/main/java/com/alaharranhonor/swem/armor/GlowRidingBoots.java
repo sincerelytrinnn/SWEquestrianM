1189f675d7fc3f8461b35b6bee3cef337acf6663
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-14 09:37:35 +0200

    init GRB

--- src/main/java/com/alaharranhonor/swem/armor/GlowRidingBoots.java
+++ src/main/java/com/alaharranhonor/swem/armor/GlowRidingBoots.java
@@ -1,2 +1,33 @@
-package com.alaharranhonor.swem.armor;public class GlowRidingBoots {
+package com.alaharranhonor.swem.armor;
+
+import com.alaharranhonor.swem.util.RegistryHandler;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class GlowRidingBoots extends ArmorItem {
+	public GlowRidingBoots(IArmorMaterial materialIn, EquipmentSlotType slot, Properties builderIn) {
+		super(materialIn, slot, builderIn);
+	}
+
+	@Override
+	public void onCreated(ItemStack stack, World worldIn, PlayerEntity playerIn) {
+		stack.addEnchantment(RegistryHandler.UPSTEP.get(), 1);
+		super.onCreated(stack, worldIn, playerIn);
+	}
+
+	/**
+	 * Called to tick armor in the armor slot. Override to do something
+	 *
+	 * @param stack
+	 * @param world
+	 * @param player
+	 */
+	@Override
+	public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
+		// Add glow to the player.
+	}
 }
