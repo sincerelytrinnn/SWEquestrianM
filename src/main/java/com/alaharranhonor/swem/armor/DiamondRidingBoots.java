c28f5563838b6831ba523b49a0313954d2e7aaff
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-14 09:37:22 +0200

    added DRB, and made bonusses stack. Also made the armor, take 1 durability hit, when burning every second.

--- src/main/java/com/alaharranhonor/swem/armor/DiamondRidingBoots.java
+++ src/main/java/com/alaharranhonor/swem/armor/DiamondRidingBoots.java
@@ -1,2 +1,59 @@
-package com.alaharranhonor.swem.armor;public class DiamondRidingBoots {
+package com.alaharranhonor.swem.armor;
+
+import com.alaharranhonor.swem.SWEM;
+import com.alaharranhonor.swem.util.RegistryHandler;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.Potions;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.world.World;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+public class DiamondRidingBoots extends ArmorItem {
+
+	private int tickDurability = 0;
+	public DiamondRidingBoots(IArmorMaterial materialIn, EquipmentSlotType slot, Properties builderIn) {
+		super(materialIn, slot, builderIn);
+	}
+
+	@Override
+	public void onCreated(ItemStack stack, World worldIn, PlayerEntity playerIn) {
+		stack.addEnchantment(RegistryHandler.UPSTEP.get(), 1);
+		stack.addEnchantment(RegistryHandler.DESTRIER.get(), 1);
+		super.onCreated(stack, worldIn, playerIn);
+	}
+
+
+
+	/**
+	 * Called to tick armor in the armor slot. Override to do something
+	 *
+	 * @param stack
+	 * @param world
+	 * @param player
+	 */
+	@Override
+	public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
+		player.addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, 1, 2));
+		if (player.isBurning()) {
+			if (this.tickDurability == 20) {
+				stack.damageItem(1, player, (broken) -> {
+					broken.sendBreakAnimation(EquipmentSlotType.FEET);
+				});
+				this.tickDurability = 0;
+			} else {
+				this.tickDurability++;
+			}
+		}
+	}
+
+
 }
