fa407ea8a216284456aae1112f15785a4f180e2e
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-17 15:37:00 +0200

    adds the configured feature, to the biome, and checks the biome.

---
+++ src/main/java/com/alaharranhonor/swem/world/gen/SWEMOreGen.java
@@ -0,0 +1,39 @@
+package com.alaharranhonor.swem.world.gen;
+
+import com.alaharranhonor.swem.util.RegistryHandler;
+import net.minecraft.block.OreBlock;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+
+public class SWEMOreGen {
+	public static ConfiguredFeature<?, ?> AMETHYST_ORE;
+	public static ConfiguredFeature<?, ?> CANTAZARITE_ORE;
+
+	public static boolean checkAndInitBiome(BiomeLoadingEvent event) {
+		if (event.getCategory() == Biome.Category.NETHER) {
+			return true;
+		}
+
+		if (event.getCategory() != Biome.Category.THEEND) {
+			initOverWorldFeatures();
+			return true;
+		}
+		return false;
+	}
+
+	protected static void initOverWorldFeatures() {
+		if (AMETHYST_ORE == null) {
+			AMETHYST_ORE = OreGenUtils.buildOverWorldFeature(RegistryHandler.AMETHYST_ORE.get().getDefaultState());
+		}
+		if (CANTAZARITE_ORE == null) {
+			CANTAZARITE_ORE = OreGenUtils.buildOverWorldFeature(RegistryHandler.CANTAZARITE_ORE.get().getDefaultState());
+		}
+	}
+
+	public static void generateOverworldOres(BiomeLoadingEvent event) {
+		event.getGeneration().withFeature(GenerationStage.Decoration.UNDERGROUND_ORES, AMETHYST_ORE);
+		event.getGeneration().withFeature(GenerationStage.Decoration.UNDERGROUND_ORES, CANTAZARITE_ORE);
+	}
+}
