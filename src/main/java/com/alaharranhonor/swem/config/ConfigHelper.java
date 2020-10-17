a147da8f0008a5c6c117d48391a43bfa256aa76d
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-17 15:37:54 +0200

    insane config type casts, to modify the field, because vanilla holds on the the object.

--- src/main/java/com/alaharranhonor/swem/config/ConfigHelper.java
+++ src/main/java/com/alaharranhonor/swem/config/ConfigHelper.java
@@ -1,14 +1,38 @@
 package com.alaharranhonor.swem.config;
 
+import com.alaharranhonor.swem.util.RegistryHandler;
+import com.alaharranhonor.swem.world.gen.SWEMOreGen;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.placement.DecoratedPlacementConfig;
+import net.minecraft.world.gen.placement.TopSolidRangeConfig;
 import net.minecraftforge.fml.config.ModConfig;
 
 public final class ConfigHelper {
+
+
 	public static void bakeClient(final ModConfig config) {
 		//
 	}
 
 	public static void bakeServer(final ModConfig config) {
-		SWLConfig.veinSize = ConfigHolder.SERVER.veinSize.get();
-		SWLConfig.veinPerChunk = ConfigHolder.SERVER.veinPerChunk.get();
+
+		// Vein Size
+		((OreFeatureConfig)((DecoratedFeatureConfig)((DecoratedFeatureConfig) SWEMOreGen.AMETHYST_ORE.config).feature.get().config).feature.get().config).size = ConfigHolder.SERVER.serverAmethystVeinSize.get();
+		// Vein Count
+		((FeatureSpreadConfig)((DecoratedFeatureConfig) SWEMOreGen.AMETHYST_ORE.config).decorator.config).func_242799_a().field_242250_b = ConfigHolder.SERVER.serverAmethystVeinCount.get();
+		// Minimum Height the ore can spawn
+		((TopSolidRangeConfig)((DecoratedPlacementConfig)((DecoratedFeatureConfig)((DecoratedFeatureConfig) SWEMOreGen.AMETHYST_ORE.config).feature.get().config).decorator.config).field_242885_d.config).field_242813_c = ConfigHolder.SERVER.serverAmethystBottomHeight.get();
+		// Maximum Height the ore can spawn.
+		((TopSolidRangeConfig)((DecoratedPlacementConfig)((DecoratedFeatureConfig)((DecoratedFeatureConfig) SWEMOreGen.AMETHYST_ORE.config).feature.get().config).decorator.config).field_242885_d.config).field_242815_e = ConfigHolder.SERVER.serverAmethystMaxHeight.get();
+
+
+		// Vein Size
+		((OreFeatureConfig)((DecoratedFeatureConfig)((DecoratedFeatureConfig) SWEMOreGen.CANTAZARITE_ORE.config).feature.get().config).feature.get().config).size = ConfigHolder.SERVER.serverCantazariteVeinSize.get();
+		// Vein Count
+		((FeatureSpreadConfig)((DecoratedFeatureConfig) SWEMOreGen.CANTAZARITE_ORE.config).decorator.config).func_242799_a().field_242250_b = ConfigHolder.SERVER.serverCantazariteVeinCount.get();
+		// Minimum Height the ore can spawn
+		((TopSolidRangeConfig)((DecoratedPlacementConfig)((DecoratedFeatureConfig)((DecoratedFeatureConfig) SWEMOreGen.CANTAZARITE_ORE.config).feature.get().config).decorator.config).field_242885_d.config).field_242813_c = ConfigHolder.SERVER.serverCantazariteBottomHeight.get();
+		// Maximum Height the ore can spawn.
+		((TopSolidRangeConfig)((DecoratedPlacementConfig)((DecoratedFeatureConfig)((DecoratedFeatureConfig) SWEMOreGen.CANTAZARITE_ORE.config).feature.get().config).decorator.config).field_242885_d.config).field_242815_e = ConfigHolder.SERVER.serverCantazariteMaxHeight.get();
 	}
 }
