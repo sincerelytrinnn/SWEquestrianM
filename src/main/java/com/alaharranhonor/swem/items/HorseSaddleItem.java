6ee80b1d3a6c5b6355ad89f9c945d70ddd10dfd4
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-19 16:34:00 +0200

    Make them their own saddle type, so vanilla doesn't work with SWEM horses.

--- src/main/java/com/alaharranhonor/swem/items/HorseSaddleItem.java
+++ src/main/java/com/alaharranhonor/swem/items/HorseSaddleItem.java
@@ -1,31 +1,35 @@
 package com.alaharranhonor.swem.items;
 
+import net.minecraft.entity.IEquipable;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.SaddleItem;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
-import net.minecraft.util.datafix.fixes.HorseSaddle;
+import net.minecraft.util.SoundCategory;
 
 
-public class HorseSaddleItem extends SaddleItem {
+public class HorseSaddleItem extends Item {
 
 
 	public HorseSaddleItem(Properties properties) {
 		super(properties);
 	}
 
-	/**
-	 * Returns true if the item can be used on the given entity, e.g. shears on sheep.
-	 *
-	 * @param stack
-	 * @param playerIn
-	 * @param target
-	 * @param hand
-	 */
-	@Override
 	public ActionResultType itemInteractionForEntity(ItemStack stack, PlayerEntity playerIn, LivingEntity target, Hand hand) {
-		return super.itemInteractionForEntity(stack, playerIn, target, hand);
+		if (target instanceof IEquipable && target.isAlive()) {
+			IEquipable iequipable = (IEquipable)target;
+			if (!iequipable.isHorseSaddled() && iequipable.func_230264_L__()) {
+				if (!playerIn.world.isRemote) {
+					iequipable.func_230266_a_(SoundCategory.NEUTRAL);
+					stack.shrink(1);
+				}
+
+				return ActionResultType.func_233537_a_(playerIn.world.isRemote);
+			}
+		}
+
+		return ActionResultType.PASS;
 	}
 }
