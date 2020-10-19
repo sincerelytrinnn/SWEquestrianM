80710d3791f24ab2020f33f5dd873af70e762f78
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-19 15:23:31 +0200

    cleaned imports

--- src/main/java/com/alaharranhonor/swem/util/ClientEventBusSubscriber.java
+++ src/main/java/com/alaharranhonor/swem/util/ClientEventBusSubscriber.java
@@ -1,17 +1,13 @@
 package com.alaharranhonor.swem.util;
 
-//import com.alaharranhonor.swem.entity.render.SWEMHorseRender;
-import com.alaharranhonor.swem.container.SWEMHorseInventoryContainer;
 import com.alaharranhonor.swem.entity.gui.SWEMHorseInventoryScreen;
 import com.alaharranhonor.swem.entity.render.SWEMHorseRender;
 import com.alaharranhonor.swem.init.SWLBlocks;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
-import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -21,7 +17,6 @@ import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import com.alaharranhonor.swem.SWEM;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod.EventBusSubscriber(modid = SWEM.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -30,14 +25,11 @@ public class ClientEventBusSubscriber {
     public static void onClientSetup(FMLClientSetupEvent event)
     {
         DeferredWorkQueue.runLater(ClientEventBusSubscriber::initLate);
-
         RenderingRegistry.registerEntityRenderingHandler(RegistryHandler.SWEM_HORSE_ENTITY.get(), SWEMHorseRender::new);
         RenderTypeLookup.setRenderLayer(RegistryHandler.TIMOTHY_GRASS.get(), RenderType.getCutout());
         RenderTypeLookup.setRenderLayer(RegistryHandler.OAT_PLANT.get(), RenderType.getCutout());
         RenderTypeLookup.setRenderLayer(RegistryHandler.ALFALFA_PLANT.get(), RenderType.getCutout());
         RenderTypeLookup.setRenderLayer(RegistryHandler.RIDING_DOOR.get(), RenderType.getCutout());
-
-
     }
 
     public static void initLate() {
