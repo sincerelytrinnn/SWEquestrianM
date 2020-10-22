fc9be3838bb845138540dd17a74e5f409d480977
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-22 16:01:21 +0200

    added blockstate prop for water troughs

--- src/main/java/com/alaharranhonor/swem/blocks/SWEMBlockStateProperties.java
+++ src/main/java/com/alaharranhonor/swem/blocks/SWEMBlockStateProperties.java
@@ -5,4 +5,5 @@ import net.minecraft.state.properties.BlockStateProperties;
 
 public class SWEMBlockStateProperties extends BlockStateProperties {
 	public static final EnumProperty<HitchingPostBase.PostPart> POST_PART = EnumProperty.create("post_part", HitchingPostBase.PostPart.class);
+	public static final EnumProperty<WaterTroughBase.TroughPart> TROUGH_PART = EnumProperty.create("trough_part", WaterTroughBase.TroughPart.class);
 }
