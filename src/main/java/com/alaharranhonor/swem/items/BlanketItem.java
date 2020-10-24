364eaf60bf5e6953e39b04f2647d89c90a07c8b3
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-24 07:33:45 +0200

    make blanket shift right clickable.

--- src/main/java/com/alaharranhonor/swem/items/BlanketItem.java
+++ src/main/java/com/alaharranhonor/swem/items/BlanketItem.java
@@ -1,9 +1,31 @@
 package com.alaharranhonor.swem.items;
 
+import com.alaharranhonor.swem.entities.ISWEMEquipable;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
 
 public class BlanketItem extends Item {
 	public BlanketItem(Properties properties) {
 		super(properties);
+	}
+
+	public ActionResultType itemInteractionForEntity(ItemStack stack, PlayerEntity playerIn, LivingEntity target, Hand hand) {
+		if (target instanceof ISWEMEquipable && target.isAlive()) {
+			ISWEMEquipable iequipable = (ISWEMEquipable)target;
+			if (!iequipable.hasBlanket() && iequipable.func_230264_L__()) {
+				if (!playerIn.world.isRemote) {
+					iequipable.func_230266_a_(SoundCategory.NEUTRAL, stack);
+					stack.shrink(1);
+				}
+
+				return ActionResultType.func_233537_a_(playerIn.world.isRemote);
+			}
+		}
+		return ActionResultType.PASS;
 	}
 }
