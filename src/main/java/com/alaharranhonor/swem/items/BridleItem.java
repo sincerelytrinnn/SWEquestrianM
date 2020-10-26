2404ffb4f6189b5cef0c86bbdc959ce0d62765a3
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-26 14:39:00 +0100

    addec custom tack items.

--- src/main/java/com/alaharranhonor/swem/items/BridleItem.java
+++ src/main/java/com/alaharranhonor/swem/items/BridleItem.java
@@ -13,14 +13,14 @@ import net.minecraft.util.SoundCategory;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class BlanketItem extends Item {
+public class BridleItem extends Item {
 
 	private final ResourceLocation texture;
-	public BlanketItem(String textureName, Properties properties) {
-		this(new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/blanket/" + textureName + ".png"), properties);
+	public BridleItem(String textureName, Properties properties) {
+		this(new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/bridle/" + textureName + ".png"), properties);
 
 	}
-	public BlanketItem(ResourceLocation texture, Properties properties) {
+	public BridleItem(ResourceLocation texture, Properties properties) {
 		super(properties);
 		this.texture = texture;
 	}
@@ -28,7 +28,7 @@ public class BlanketItem extends Item {
 	public ActionResultType itemInteractionForEntity(ItemStack stack, PlayerEntity playerIn, LivingEntity target, Hand hand) {
 		if (target instanceof ISWEMEquipable && target.isAlive()) {
 			ISWEMEquipable iequipable = (ISWEMEquipable)target;
-			if (!iequipable.hasBlanket() && iequipable.func_230264_L__()) {
+			if (!iequipable.hasBridle() && iequipable.func_230264_L__()) {
 				if (!playerIn.world.isRemote) {
 					iequipable.func_230266_a_(SoundCategory.NEUTRAL, stack);
 					stack.shrink(1);
--- src/main/java/com/alaharranhonor/swem/items/LegWrapsItem.java
+++ src/main/java/com/alaharranhonor/swem/items/LegWrapsItem.java
@@ -13,14 +13,14 @@ import net.minecraft.util.SoundCategory;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class BlanketItem extends Item {
+public class LegWrapsItem extends Item {
 
 	private final ResourceLocation texture;
-	public BlanketItem(String textureName, Properties properties) {
-		this(new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/blanket/" + textureName + ".png"), properties);
+	public LegWrapsItem(String textureName, Properties properties) {
+		this(new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/leg_wraps/" + textureName + ".png"), properties);
 
 	}
-	public BlanketItem(ResourceLocation texture, Properties properties) {
+	public LegWrapsItem(ResourceLocation texture, Properties properties) {
 		super(properties);
 		this.texture = texture;
 	}
@@ -28,7 +28,7 @@ public class BlanketItem extends Item {
 	public ActionResultType itemInteractionForEntity(ItemStack stack, PlayerEntity playerIn, LivingEntity target, Hand hand) {
 		if (target instanceof ISWEMEquipable && target.isAlive()) {
 			ISWEMEquipable iequipable = (ISWEMEquipable)target;
-			if (!iequipable.hasBlanket() && iequipable.func_230264_L__()) {
+			if (!iequipable.hasLegWraps() && iequipable.func_230264_L__()) {
 				if (!playerIn.world.isRemote) {
 					iequipable.func_230266_a_(SoundCategory.NEUTRAL, stack);
 					stack.shrink(1);
