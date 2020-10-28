a3f869f7c0c8a225d1958a6bfa2ae0591807cef6
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-10-28 14:44:58 +0100

    added registerBlockColors

--- src/main/java/com/alaharranhonor/swem/util/ClientEventBusSubscriber.java
+++ src/main/java/com/alaharranhonor/swem/util/ClientEventBusSubscriber.java
@@ -2,12 +2,16 @@ package com.alaharranhonor.swem.util;
 
 import com.alaharranhonor.swem.entity.gui.SWEMHorseInventoryScreen;
 import com.alaharranhonor.swem.entity.render.SWEMHorseRender;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.color.BlockColors;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.world.biome.BiomeColors;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DeferredWorkQueue;
@@ -29,9 +33,20 @@ public class ClientEventBusSubscriber {
         RenderTypeLookup.setRenderLayer(RegistryHandler.OAT_PLANT.get(), RenderType.getCutout());
         RenderTypeLookup.setRenderLayer(RegistryHandler.ALFALFA_PLANT.get(), RenderType.getCutout());
         RenderTypeLookup.setRenderLayer(RegistryHandler.RIDING_DOOR.get(), RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(RegistryHandler.HALF_BARREL.get(), RenderType.getTranslucent());
     }
 
     public static void initLate() {
         ScreenManager.registerFactory(RegistryHandler.SWEM_HORSE_CONTAINER.get(), SWEMHorseInventoryScreen::new);
     }
+
+    @SubscribeEvent
+    public static void onRegisterBlockColors(ColorHandlerEvent.Block event) {
+        BlockColors colors = event.getBlockColors();
+        colors.register((state, reader, pos, color) -> {
+            return reader != null && pos != null ? BiomeColors.getWaterColor(reader, pos) : -1;
+        }, RegistryHandler.HALF_BARREL.get());
+    }
+
+
 }
