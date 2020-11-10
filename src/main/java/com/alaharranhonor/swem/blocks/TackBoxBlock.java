1c820209dd8a896c1d0f734c17495ad9009a395c
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-11-10 19:43:19 +0100

    fixed voxel shape for now

--- src/main/java/com/alaharranhonor/swem/blocks/TackBoxBlock.java
+++ src/main/java/com/alaharranhonor/swem/blocks/TackBoxBlock.java
@@ -15,6 +15,9 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
@@ -80,6 +83,11 @@ public class TackBoxBlock extends Block {
 				InventoryHelper.dropItems(worldIn, pos, ((TackBoxTE)te).getItems());
 			}
 		}
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		return VoxelShapes.create(0.01d, 0.01d, 0.01d, 15.99d, 15.99d, 15.99);
 	}
 
 	/**
