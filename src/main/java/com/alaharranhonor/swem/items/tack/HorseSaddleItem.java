f48d2c0ae5144d548c85def8400762c40f42ff94
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-11-18 12:09:38 +0100

    added depnendcy for each tack.

--- src/main/java/com/alaharranhonor/swem/items/tack/HorseSaddleItem.java
+++ src/main/java/com/alaharranhonor/swem/items/tack/HorseSaddleItem.java
@@ -2,6 +2,7 @@ package com.alaharranhonor.swem.items.tack;
 
 import com.alaharranhonor.swem.SWEM;
 import com.alaharranhonor.swem.entities.ISWEMEquipable;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.entity.IEquipable;
 import net.minecraft.entity.LivingEntity;
@@ -13,6 +14,7 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.text.StringTextComponent;
 
 import javax.annotation.Nullable;
 
@@ -30,7 +32,11 @@ public class HorseSaddleItem extends Item {
 	public ActionResultType itemInteractionForEntity(ItemStack stack, PlayerEntity playerIn, LivingEntity target, Hand hand) {
 		if (target instanceof ISWEMEquipable && target.isAlive()) {
 			ISWEMEquipable iequipable = (ISWEMEquipable)target;
-			if (!iequipable.isHorseSaddled() && iequipable.func_230264_L__()) {
+			if (playerIn.world.isRemote && !iequipable.canEquipSaddle()) {
+				playerIn.sendStatusMessage(new StringTextComponent("You need to equip a Blanket first!"), true);
+				return ActionResultType.FAIL;
+			}
+			if (!iequipable.isHorseSaddled() && iequipable.func_230264_L__() && iequipable.canEquipSaddle()) {
 				if (!playerIn.world.isRemote) {
 					iequipable.func_230266_a_(SoundCategory.NEUTRAL, stack);
 					stack.shrink(1);
