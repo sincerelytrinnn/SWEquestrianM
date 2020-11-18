ad115f28c3bf9f54e7fa883905bd0a86701db919
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-11-18 20:28:10 +0100

    Added effect, for the potion when right clicking, a vanilla horse. It turns into SWEM Horse.

--- src/main/java/com/alaharranhonor/swem/items/potions/CantazaritePotionItem.java
+++ src/main/java/com/alaharranhonor/swem/items/potions/CantazaritePotionItem.java
@@ -1,8 +1,11 @@
 package com.alaharranhonor.swem.items.potions;
 
 import com.alaharranhonor.swem.SWEM;
+import com.alaharranhonor.swem.entities.SWEMHorseEntity;
+import com.alaharranhonor.swem.util.initialization.SWEMEntities;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.passive.horse.HorseEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -13,9 +16,11 @@ import net.minecraft.potion.Potion;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -29,8 +34,13 @@ public class CantazaritePotionItem extends PotionItem {
 
 	public ActionResultType itemInteractionForEntity(ItemStack stack, PlayerEntity playerIn, LivingEntity target, Hand hand) {
 		if (target instanceof HorseEntity) {
-			SWEM.LOGGER.info("Triggered");
-
+			if (!playerIn.world.isRemote) {
+				BlockPos targetPos = target.getPosition();
+				target.remove();
+				SWEMHorseEntity horse1 = (SWEMHorseEntity) SWEMEntities.SWEM_HORSE_ENTITY.get().spawn((ServerWorld) playerIn.world, null, playerIn, targetPos, SpawnReason.MOB_SUMMONED, true, false);
+			}
+			stack.shrink(1);
+			return ActionResultType.func_233537_a_(playerIn.world.isRemote);
 		}
 		return ActionResultType.PASS;
 	}
