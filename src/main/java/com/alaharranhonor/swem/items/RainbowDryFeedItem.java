c57b497a4c85c2bbc8e52a22655f7051f25226a4
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-01-11 11:36:05 +0100

    added brewing recipes in a seperate class holder.

---
+++ src/main/java/com/alaharranhonor/swem/items/RainbowDryFeedItem.java
@@ -0,0 +1,25 @@
+package com.alaharranhonor.swem.items;
+
+import com.alaharranhonor.swem.util.initialization.SWEMItems;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+
+public class RainbowDryFeedItem extends ItemBase {
+
+	private int counter = 0;
+
+	@Override
+	public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+		if (!isSelected) return;
+		if (!worldIn.getWorldInfo().isRaining()) return;
+		counter++;
+		if (counter % 80 == 0) {
+			PlayerEntity entity = (PlayerEntity) entityIn;
+			ItemStack din = new ItemStack(SWEMItems.RAINBOW_DINDIN.get());
+			entity.setHeldItem(Hand.MAIN_HAND, din);
+		}
+	}
+}
