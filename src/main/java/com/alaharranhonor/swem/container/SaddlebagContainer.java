e6fd6971f82084035c83d6b375f6c92cc473fee6
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-01-19 18:55:33 +0100

    fixed isItemValid for all slots

--- src/main/java/com/alaharranhonor/swem/container/SaddlebagContainer.java
+++ src/main/java/com/alaharranhonor/swem/container/SaddlebagContainer.java
@@ -1,14 +1,32 @@
 package com.alaharranhonor.swem.container;
 
+import com.alaharranhonor.swem.blocks.HalfBarrelBlock;
+import com.alaharranhonor.swem.blocks.HitchingPostBase;
 import com.alaharranhonor.swem.entities.SWEMHorseEntityBase;
+import com.alaharranhonor.swem.items.HoseItem;
+import com.alaharranhonor.swem.items.tack.BridleItem;
+import com.alaharranhonor.swem.items.tack.HalterItem;
+import com.alaharranhonor.swem.util.initialization.SWEMBlocks;
 import com.alaharranhonor.swem.util.initialization.SWEMContainers;
+import com.alaharranhonor.swem.util.initialization.SWEMItems;
+import com.google.common.collect.Lists;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.CraftingTableBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.Slot;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.network.PacketBuffer;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.data.ForgeItemTagsProvider;
+
+import java.util.ArrayList;
 
 public class SaddlebagContainer extends Container {
 	private final IInventory horseInventory;
@@ -27,21 +45,330 @@ public class SaddlebagContainer extends Container {
 
 
 		int startSaddlebagInvY = 18;
-		for (int row = 0; row < 3; ++row) {
-			for (int col = 0; col < 9; ++col) {
-				this.addSlot(new Slot(horseInventory, (row * 9) + col, 8 + (col * 18), startSaddlebagInvY + (row * 18)) {
-					@Override
-					public boolean isItemValid(ItemStack stack) {
-						return super.isItemValid(stack);
-					}
 
-					@Override
-					public int getSlotStackLimit() {
-						return super.getSlotStackLimit();
-					}
-				});
+		this.addSlot(new Slot(horseInventory, 0, 8, startSaddlebagInvY) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof HalterItem;
 			}
-		}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 1, 26, startSaddlebagInvY) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof HoseItem;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 2, 44, startSaddlebagInvY) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return  Block.getBlockFromItem(stack.getItem()) instanceof HitchingPostBase;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 3, 62, startSaddlebagInvY) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof AirItem;
+			} // TODO: Fly spray
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 4, 80, startSaddlebagInvY) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() == SWEMItems.SUGAR_CUBE.get();
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 5, 98, startSaddlebagInvY) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() == SWEMBlocks.QUALITY_BALE_ITEM.get();
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 6, 116, startSaddlebagInvY) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof AirItem;
+			} // TODO: SWEET FEED
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 7, 134, startSaddlebagInvY) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof AirItem;
+			} // TODO: BRUSH
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 8, 152, startSaddlebagInvY) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem().getTranslationKey().equals(SWEMBlocks.HALF_BARREL_ITEM.get().getTranslationKey());
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 9, 8, startSaddlebagInvY + 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() == Blocks.CRAFTING_TABLE.asItem();
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 10, 26, startSaddlebagInvY + 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem().isIn(ItemTags.LOGS);
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 11, 44, startSaddlebagInvY + 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() == Items.TORCH;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 12, 62, startSaddlebagInvY + 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() == Items.BEEF || stack.getItem() == Items.COOKED_BEEF;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 13, 80, startSaddlebagInvY + 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() == Items.CHEST;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 14, 98, startSaddlebagInvY + 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem().isIn(Tags.Items.INGOTS);
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 15, 116, startSaddlebagInvY + 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof BlockItem;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 16, 134, startSaddlebagInvY + 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof BlockItem;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 17, 152, startSaddlebagInvY + 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof BucketItem;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 18, 8, startSaddlebagInvY + 2 * 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof SwordItem;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 19, 26, startSaddlebagInvY + 2 * 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof PickaxeItem;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 20, 44, startSaddlebagInvY + 2 * 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof AxeItem;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 21, 62, startSaddlebagInvY + 2 * 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof ShovelItem;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 22, 80, startSaddlebagInvY + 2 * 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof BowItem;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 23, 98, startSaddlebagInvY + 2 * 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof ArrowItem;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 24, 116, startSaddlebagInvY + 2 * 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() instanceof ShieldItem;
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 25, 134, startSaddlebagInvY + 2 * 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() == SWEMItems.CANTAZARITE.get();
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
+
+		this.addSlot(new Slot(horseInventory, 26, 152, startSaddlebagInvY + 2 * 18) {
+			@Override
+			public boolean isItemValid(ItemStack stack) {
+				return stack.getItem() == SWEMBlocks.CANTAZARITE_ANVIL.get().asItem();
+			}
+
+			@Override
+			public int getSlotStackLimit() {
+				return super.getSlotStackLimit();
+			}
+		});
 
 		// Player Main Inventory
 		int startPlayerInvY = 84;
@@ -80,7 +407,12 @@ public class SaddlebagContainer extends Container {
 				if (!this.mergeItemStack(itemstack1, i, this.inventorySlots.size(), true)) {
 					return ItemStack.EMPTY;
 				}
-			} else if (this.getSlot(6).isItemValid(itemstack1) && !this.getSlot(6).getHasStack()) {
+			}
+
+			// Loop over the slots.
+
+
+			else if (this.getSlot(6).isItemValid(itemstack1) && !this.getSlot(6).getHasStack()) {
 				if (!this.mergeItemStack(itemstack1, 6, 7, false)) {
 					return ItemStack.EMPTY;
 				}
