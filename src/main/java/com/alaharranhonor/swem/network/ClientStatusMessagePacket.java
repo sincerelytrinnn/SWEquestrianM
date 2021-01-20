47bbbd967fc09e05dfdb63c298be1a6db25ba7f0
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-01-20 14:52:09 +0100

    fixed translation text, for gallop cooldown

--- src/main/java/com/alaharranhonor/swem/network/ClientStatusMessagePacket.java
+++ src/main/java/com/alaharranhonor/swem/network/ClientStatusMessagePacket.java
@@ -57,7 +57,7 @@ public class ClientStatusMessagePacket {
 		ctx.get().enqueueWork(() -> {
 			switch (msg.action) {
 				case 0: {
-					Minecraft.getInstance().player.sendStatusMessage(new TranslationTextComponent("swem.horse.status.gallop_cooldown ").appendString( msg.args.get(0) + "s"),true);
+					Minecraft.getInstance().player.sendStatusMessage(new TranslationTextComponent("swem.horse.status.gallop_cooldown").appendString( msg.args.get(0) + "s"),true);
 					break;
 				}
 				case 1: {
