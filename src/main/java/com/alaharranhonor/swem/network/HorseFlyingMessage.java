5ac2d0cb6856a1d6f5c59307a35151af68007d93
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-01-26 21:56:18 +0100

    added flying horse change packet.

---
+++ src/main/java/com/alaharranhonor/swem/network/HorseFlyingMessage.java
@@ -0,0 +1,90 @@
+/*
+package com.alaharranhonor.swem.network;
+
+import com.alaharranhonor.swem.SWEM;
+import com.alaharranhonor.swem.entities.SWEMHorseEntityBase;
+import com.alaharranhonor.swem.util.initialization.SWEMItems;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.network.NetworkHooks;
+
+import javax.annotation.Nullable;
+import java.util.function.Supplier;
+
+public class HorseFlyingMessage {
+
+	private int action;
+
+	private boolean failed;
+
+	public HorseFlyingMessage(int action) {
+		this.action = action;
+		this.failed = false;
+	}
+
+	public HorseFlyingMessage(boolean failed) {
+		this.failed = failed;
+	}
+
+	public static HorseFlyingMessage decode(ByteBuf buf) {
+		try {
+			int action = buf.readInt();
+			return new HorseFlyingMessage(action);
+		} catch (IndexOutOfBoundsException e) {
+			SWEM.LOGGER.error("HorseFlyingMessage: Unexpected end of packet.\nMessage: " + ByteBufUtil.hexDump(buf, 0, buf.writerIndex()), e);
+			return new HorseFlyingMessage(true);
+		}
+	}
+
+	public static void encode(HorseFlyingMessage msg, PacketBuffer buffer) {
+		buffer.writeInt(msg.action);
+	}
+
+	public static void handle(HorseFlyingMessage msg, Supplier<NetworkEvent.Context> ctx) {
+		ctx.get().enqueueWork(() -> {
+			ServerPlayerEntity player = ctx.get().getSender();
+			if (player.isPassenger() && player.getRidingEntity() instanceof SWEMHorseEntityBase) {
+				SWEMHorseEntityBase horse = (SWEMHorseEntityBase) player.getRidingEntity();
+				switch (msg.action) {
+
+					case 0: {
+						break;
+					}
+					case 1: {
+						if (horse.isFlying()) {
+							horse.setMotion(new Vector3d(0.0D, 1.0D, 0.0D));
+						}
+						break;
+					}
+					case 2: {
+						horse.setFlying(!horse.isFlying());
+						SWEM.LOGGER.debug("Horse is now flying: " + horse.isFlying());
+						break;
+					}
+					case 3: {
+						if (horse.isFlying()) {
+							horse.setMotion(player.getLookVec().scale(3));
+						}
+						break;
+					}
+
+				}
+			}
+
+
+		});
+		ctx.get().setPacketHandled(true);
+	}
+}
+*/
