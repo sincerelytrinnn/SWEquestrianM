0518a13aff41ea64ff280be494077edeb0027bee
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-04-15 23:30:49 +0200

    finetuned the rotations and translations to match up with the rack

--- src/main/java/com/alaharranhonor/swem/entity/render/HorseArmorRackRender.java
+++ src/main/java/com/alaharranhonor/swem/entity/render/HorseArmorRackRender.java
@@ -17,6 +17,7 @@ import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.entity.item.minecart.MinecartEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Direction;
@@ -55,34 +56,55 @@ public class HorseArmorRackRender extends GeoBlockRenderer<HorseArmorRackTE> {
 			GeoBone bone = (GeoBone) group.next();
 
 
-			// Childbones are inverted in order, from what shown in BlockBench.
+			// Childbones are inverted in order for some bones, from what shown in BlockBench.
+			// the order of the bones are stored in a json array which does not guarantee order preservation - Gecko
 
+			if (bone.getName().equals("cloth")) {
+				GeoBone body = bone.childBones.get(0);
+				body.setPositionY(3);
+			}
 			if (bone.getName().equals("iron")) {
 				GeoBone head = bone.childBones.get(0);
-				bone.setPositionY(-10);
-				head.setPositionY(-11);
+				bone.setPositionY(-15);
+				bone.setPositionX(1);
+
+
+				head.setPivotY(32);
+				head.setPivotX(19);
+				head.setPositionY(-1);
+				head.setPositionX(0);
+				head.setRotationZ(-1.575f); // Rotations are apparently not in degrees. Also this rotation is dependent on the current pivot points and positions.
 
 			} else if (bone.getName().equals("gold"))
 			{
 				GeoBone feet = bone.childBones.get(0);
 				GeoBone head = bone.childBones.get(1);
 
-				bone.setPositionY(-10);
-				head.setPositionY(-11);
-				feet.setPositionY(12);
+				bone.setPositionY(-15);
+				bone.setPositionX(1);
+
+
+				head.setPivotY(32);
+				head.setPivotX(19);
+				head.setPositionY(-1);
+				head.setPositionX(0);
+				head.setRotationZ(-1.575f); // Rotations are apparently not in degrees. Also this rotation is dependent on the current pivot points and positions.
+				feet.setPositionY(17);
 
 
 			} else if (bone.getName().equals("diamond")) {
 
 				GeoBone neck = bone.childBones.get(0);
-				float neckY = neck.getPositionY();
 
-				bone.setPositionY(-10);
+				bone.setPositionY(-15);
+				bone.setPositionX(1);
 				neck.setRotationZ(0);
 				neck.setPositionY(-7);
+				neck.setPositionX(-4);
 
 			} else {
-				bone.setPositionY(-10);
+				bone.setPositionY(-15);
+				bone.setPositionX(1);
 			}
 
 			stack.push();
