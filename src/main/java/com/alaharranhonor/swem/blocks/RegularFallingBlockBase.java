0f99d4d3de25872d673c82eee18db9a3ca007833
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-04-21 12:19:57 +0200

    remove unused classes

--- src/main/java/com/alaharranhonor/swem/blocks/RegularFallingBlockBase.java
+++
@@ -1,9 +0,0 @@
-package com.alaharranhonor.swem.blocks;
-
-import net.minecraft.block.FallingBlock;
-
-public class RegularFallingBlockBase extends FallingBlock {
-    public RegularFallingBlockBase(Properties properties) {
-        super(properties);
-    }
-}
