410509f887cbf0a722294ade1364186e3c806207
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-05-05 14:31:59 +0200

    removed items

--- src/main/java/com/alaharranhonor/swem/items/HorseTransformItem.java
+++ src/main/java/com/alaharranhonor/swem/items/HorseTransformItem.java
@@ -1,5 +1,6 @@
 package com.alaharranhonor.swem.items;
 
+import com.alaharranhonor.swem.SWEM;
 import com.alaharranhonor.swem.entities.SWEMHorseEntity;
 import com.alaharranhonor.swem.entity.coats.SWEMCoatColors;
 import com.alaharranhonor.swem.util.initialization.SWEMEntities;
@@ -7,17 +8,19 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.passive.horse.HorseEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.server.ServerWorld;
 
-public class HorseTransformItem extends ItemBase {
+public class HorseTransformItem extends Item {
 
 	private SWEMCoatColors coat;
 
 	public HorseTransformItem(SWEMCoatColors coat) {
+		super(new Item.Properties());
 		this.coat = coat;
 	}
 
