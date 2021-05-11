0bc913f71f4773f36f0b923e468c07a62958d405
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-05-11 17:22:35 +0200

    added a invisible jump controller block

---
+++ src/main/java/com/alaharranhonor/swem/blocks/jumps/JumpControllerBlock.java
@@ -0,0 +1,54 @@
+package com.alaharranhonor.swem.blocks.jumps;
+
+import com.alaharranhonor.swem.util.initialization.SWEMTileEntities;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.StateContainer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
+
+import javax.annotation.Nullable;
+
+public class JumpControllerBlock extends HorizontalBlock {
+	public JumpControllerBlock(Properties properties) {
+		super(properties);
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		return VoxelShapes.create(0, 0, 0, 0.2, 0.2, 0.2);
+	}
+
+	@Override
+	public boolean isReplaceable(BlockState state, BlockItemUseContext useContext) {
+		return false;
+	}
+
+	@Override
+	public boolean hasTileEntity(BlockState state) {
+		return true;
+	}
+
+	@Nullable
+	@Override
+	public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+		return SWEMTileEntities.JUMP_TILE_ENTITY.get().create();
+	}
+
+	@Nullable
+	@Override
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		return this.getDefaultState().with(HORIZONTAL_FACING, context.getPlacementHorizontalFacing().getOpposite());
+	}
+
+	@Override
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		builder.add(HORIZONTAL_FACING);
+	}
+}
