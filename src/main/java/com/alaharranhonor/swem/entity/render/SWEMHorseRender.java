bf75925eafa2f0d252ea9e417fefa56fa9350519
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-05-14 15:18:42 +0200

    fixed the coats, for updated geckolib

--- src/main/java/com/alaharranhonor/swem/entity/render/SWEMHorseRender.java
+++ src/main/java/com/alaharranhonor/swem/entity/render/SWEMHorseRender.java
@@ -27,26 +27,7 @@ import java.util.Map;
 public class SWEMHorseRender extends GeoEntityRenderer<SWEMHorseEntity> {
 
     protected static final ResourceLocation TEXTURE = new ResourceLocation(SWEM.MOD_ID, "textures/entity/swem_horse.png");
-    private static final Map<SWEMCoatColors, ResourceLocation> VARIANTS = Util.make(Maps.newEnumMap(SWEMCoatColors.class), (iter) -> {
-        iter.put(SWEMCoatColors.WHITE, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/white_coat.png"));
-        iter.put(SWEMCoatColors.GRAY, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/gray_coat.png"));
-        iter.put(SWEMCoatColors.BLACK, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/black_coat.png"));
-        iter.put(SWEMCoatColors.DARKBROWN, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/dark_brown_coat.png"));
-        iter.put(SWEMCoatColors.BROWN, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/brown_coat.png"));
-        iter.put(SWEMCoatColors.REDROAN, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/red_roan_coat.png"));
-        iter.put(SWEMCoatColors.BUCKSKIN, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/buckskin_coat.png"));
-        iter.put(SWEMCoatColors.PAINT, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/paint_coat.png"));
-        iter.put(SWEMCoatColors.PALOMINO, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/palomino_coat.png"));
-        iter.put(SWEMCoatColors.NOBUCKLE, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/nobuckle_coat.png"));
-        iter.put(SWEMCoatColors.WILDANDFREE, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/wild_and_free_coat.png"));
-        iter.put(SWEMCoatColors.TALLDARKHANDSOME, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/tall_dark_handsome_coat.png"));
-        iter.put(SWEMCoatColors.SWEETBOY, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/sweet_boy_coat.png"));
-        iter.put(SWEMCoatColors.APPY, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/appy_coat.png"));
-        iter.put(SWEMCoatColors.GOLDEN, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/golden_coat.png"));
-        iter.put(SWEMCoatColors.LEOPARD, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/leopard_coat.png"));
-        iter.put(SWEMCoatColors.GALAXY, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/galaxy_coat.png"));
-        iter.put(SWEMCoatColors.RAINBOW, new ResourceLocation(SWEM.MOD_ID, "textures/entity/horse/coats/rainbow_coat.png"));
-    });
+
 
     public SWEMHorseRender(EntityRendererManager renderManagerIn) {
         super(renderManagerIn, new SWEMHorseModel());
@@ -73,7 +54,7 @@ public class SWEMHorseRender extends GeoEntityRenderer<SWEMHorseEntity> {
 
     @Override
     public ResourceLocation getEntityTexture(SWEMHorseEntity entity) {
-        return VARIANTS.get(entity.getCoatColor());
+        return TEXTURE;
     }
 
     @Override
