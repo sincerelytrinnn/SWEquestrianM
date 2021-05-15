eb39a9bc42afc559a26a0d6814d3eed53ff3c4c8
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-05-15 12:01:57 +0200

    added adventure tack items

---
+++ src/main/java/com/alaharranhonor/swem/items/tack/AdventureBridleItem.java
@@ -0,0 +1,7 @@
+package com.alaharranhonor.swem.items.tack;
+
+public class AdventureBridleItem extends BridleItem {
+	public AdventureBridleItem(String textureName, Properties properties) {
+		super(textureName, properties);
+	}
+}
