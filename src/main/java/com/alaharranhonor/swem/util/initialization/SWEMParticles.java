f9e3dd94137e1bd6039d2f1e8f01f494cccb763d
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-05-15 14:35:22 +0200

    registered bad particle type

--- src/main/java/com/alaharranhonor/swem/util/initialization/SWEMParticles.java
+++ src/main/java/com/alaharranhonor/swem/util/initialization/SWEMParticles.java
@@ -1,8 +1,11 @@
 package com.alaharranhonor.swem.util.initialization;
 
 import com.alaharranhonor.swem.SWEM;
+import net.minecraft.client.particle.HeartParticle;
 import net.minecraft.particles.BasicParticleType;
 import net.minecraft.particles.ParticleType;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,9 +14,10 @@ public class SWEMParticles {
 
 	public static final DeferredRegister<ParticleType<?>> PARTICLES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, SWEM.MOD_ID);
 
+	public static void init(IEventBus modBus) {
+		PARTICLES.register(modBus);
+	}
 
-	public static final RegistryObject<BasicParticleType> BAD_1 = PARTICLES.register("bad_1", () -> new BasicParticleType(false));
-	public static final RegistryObject<BasicParticleType> BAD_2 = PARTICLES.register("bad_2", () -> new BasicParticleType(false));
-	public static final RegistryObject<BasicParticleType> BAD_3 = PARTICLES.register("bad_3", () -> new BasicParticleType(false));
+	public static final RegistryObject<BasicParticleType> BAD = PARTICLES.register("bad", () -> new BasicParticleType(false));
 
 }
