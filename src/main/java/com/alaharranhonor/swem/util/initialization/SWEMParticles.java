ac752c3488492cde0860e188c2c182d7eee22624
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-05-15 18:56:02 +0200

    registered particles

--- src/main/java/com/alaharranhonor/swem/util/initialization/SWEMParticles.java
+++ src/main/java/com/alaharranhonor/swem/util/initialization/SWEMParticles.java
@@ -22,5 +22,9 @@ public class SWEMParticles {
 	// Current particle just picks a random sprite, from the sprite list.
 	// if we want particles,that animate/transition, take a look ath the SpellParticle.
 	public static final RegistryObject<BasicParticleType> BAD = PARTICLES.register("bad", () -> new BasicParticleType(false));
+	public static final RegistryObject<BasicParticleType> ECH = PARTICLES.register("ech", () -> new BasicParticleType(false));
+	public static final RegistryObject<BasicParticleType> MEH = PARTICLES.register("meh", () -> new BasicParticleType(false));
+	public static final RegistryObject<BasicParticleType> YAY = PARTICLES.register("yay", () -> new BasicParticleType(false));
+	public static final RegistryObject<BasicParticleType> WOOT = PARTICLES.register("woot", () -> new BasicParticleType(false));
 
 }
