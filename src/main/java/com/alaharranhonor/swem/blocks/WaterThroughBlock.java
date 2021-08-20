00064fa4770a805e6085588d06b64adb81bc6dfd
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-08-20 11:19:34 +0200

    limit water thorugh to 4 connecting pieces, and limit placeability, to avoid a bypass

--- src/main/java/com/alaharranhonor/swem/blocks/WaterThroughBlock.java
+++ src/main/java/com/alaharranhonor/swem/blocks/WaterThroughBlock.java
@@ -1,9 +1,11 @@
 package com.alaharranhonor.swem.blocks;
 
 
+import com.google.common.collect.Lists;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.DyeColor;
 import net.minecraft.state.IntegerProperty;
@@ -13,11 +15,17 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.GameType;
 import net.minecraft.world.IBlockReader;
 
 import net.minecraft.block.AbstractBlock.Properties;
 import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class WaterThroughBlock extends NonParallelBlock {
 	public static final IntegerProperty LEVEL = SWEMBlockStateProperties.LEVEL_0_12;
@@ -34,31 +42,40 @@ public class WaterThroughBlock extends NonParallelBlock {
 		return VoxelShapes.box(0.01, 0.01, 0.01, 0.99, 0.99, 0.99);
 	}
 
-	@Override
-	public BlockState updateShape(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
-		BlockState orgState = stateIn;
-		BlockState updated = super.updateShape(stateIn, facing, facingState, worldIn, currentPos, facingPos);
-		int connections = 0;
-		if ((orgState.getValue(NonParallelBlock.PART) == SWEMBlockStateProperties.TwoWay.LEFT) && (updated.getValue(NonParallelBlock.PART) == SWEMBlockStateProperties.TwoWay.MIDDLE)) {
-			connections = countConnections(updated, worldIn, currentPos);
-		}
 
-		if (connections > 4) {
-			return this.defaultBlockState().setValue(FACING, stateIn.getValue(FACING));
-		}
-		return updated;
-
-	}
-
-	private int countConnections(BlockState state, IWorld world, BlockPos pos) {
+	private int countConnectionsFromLeft(BlockState state, IWorldReader world, BlockPos pos) {
 		Direction dir = state.getValue(HorizontalBlock.FACING);
-		BlockPos leftPos = pos.relative(dir.getCounterClockWise());
 		int connections = 0;
-		for (int i = 0; i < 5; i++) {
-			BlockState checkState = world.getBlockState(leftPos.relative(dir.getClockWise(), i));
+		for (int i = 1; i < 5; i++) {
+			BlockState checkState = world.getBlockState(pos.relative(dir.getClockWise(), i));
+			if (checkState.isAir()) {
+				break;
+			}
 			if (checkState.getValue(NonParallelBlock.PART) != SWEMBlockStateProperties.TwoWay.RIGHT) {
 				connections++;
 			} else if (checkState.getValue(NonParallelBlock.PART) == SWEMBlockStateProperties.TwoWay.RIGHT){
+				connections++;
+				break;
+			} else {
+				break;
+			}
+		}
+		System.out.println(connections);
+		return connections;
+
+	}
+
+	private int countConnectionsFromRight(BlockState state, IWorldReader world, BlockPos pos) {
+		Direction dir = state.getValue(HorizontalBlock.FACING);
+		int connections = 0;
+		for (int i = 1; i < 5; i++) {
+			BlockState checkState = world.getBlockState(pos.relative(dir.getCounterClockWise(), i));
+			if (checkState.isAir()) {
+				break;
+			}
+			if (checkState.getValue(NonParallelBlock.PART) != SWEMBlockStateProperties.TwoWay.LEFT) {
+				connections++;
+			} else if (checkState.getValue(NonParallelBlock.PART) == SWEMBlockStateProperties.TwoWay.LEFT){
 				connections++;
 				break;
 			} else {
@@ -74,5 +91,38 @@ public class WaterThroughBlock extends NonParallelBlock {
 	@Override
 	protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
 		super.createBlockStateDefinition(builder);
+	}
+
+	@Override
+	public boolean canSurvive(BlockState state, IWorldReader world, BlockPos pos) {
+		// Check the facing, then check counter clockwise and clockwise directiosn, and offset on the blockpos.
+		// IF we encounter a Left piece, we count connections going from the left (current method)
+		// If we encounter a Right piece, we count connections going from theright (seperate method).
+		// If the connections, are 4, return false, to deny placement.
+
+		Direction facing = state.getValue(FACING);
+		BlockState checkLeft = world.getBlockState(pos.relative(facing.getClockWise()));
+		BlockState checkRight = world.getBlockState(pos.relative(facing.getCounterClockWise()));
+		BlockState checkStraight = world.getBlockState(pos.relative(facing));
+		BlockState checkBehind = world.getBlockState(pos.relative(facing.getOpposite()));
+
+		List<BlockState> states = Arrays.asList(checkLeft, checkRight, checkStraight, checkBehind);
+
+		for (BlockState checkState : states) {
+			if (!checkState.isAir() && checkState.getBlock() instanceof WaterThroughBlock) {
+				int connections = 0;
+				if (checkState.getValue(PART) == SWEMBlockStateProperties.TwoWay.LEFT) {
+					connections = countConnectionsFromLeft(checkState, world, pos);
+				} else if (checkState.getValue(PART) == SWEMBlockStateProperties.TwoWay.RIGHT) {
+					connections = countConnectionsFromRight(checkState, world, pos);
+				}
+
+				if (connections >= 4) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
 	}
 }
