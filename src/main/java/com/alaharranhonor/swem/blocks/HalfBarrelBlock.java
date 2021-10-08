420c70fd2f4519708052d431b705fa9a249dc4d1
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-10-08 12:35:26 +0200

    updated half barrel hitbox

--- src/main/java/com/alaharranhonor/swem/blocks/HalfBarrelBlock.java
+++ src/main/java/com/alaharranhonor/swem/blocks/HalfBarrelBlock.java
@@ -32,28 +32,6 @@ import net.minecraft.block.AbstractBlock.Properties;
 
 public class HalfBarrelBlock extends Block {
 	public static final IntegerProperty LEVEL = BlockStateProperties.LEVEL_CAULDRON;
-	private static final VoxelShape SHAPE = Stream.of(
-			Block.box(4.686291501015241, 11, 0, 11.31370849898476, 12, 1),
-			Block.box(4.686291501015241, 11, 0, 11.31370849898476, 12, 1),
-			Block.box(4.686291501015241, 11, 15, 11.31370849898476, 12, 16),
-			Block.box(4.686291501015241, 11, 15, 11.31370849898476, 12, 16),
-			Block.box(0, 11, 4.686291501015241, 1, 12, 11.31370849898476),
-			Block.box(0, 11, 4.686291501015241, 1, 12, 11.31370849898476),
-			Block.box(15, 11, 4.686291501015241, 16, 12, 11.31370849898476),
-			Block.box(15, 11, 4.686291501015241, 16, 12, 11.31370849898476),
-			Block.box(4.893398282201788, 1, 0.5, 11.106601717798213, 12, 1.5),
-			Block.box(4.893398282201788, 1, 0.5, 11.106601717798213, 12, 1.5),
-			Block.box(4.893398282201788, 1, 14.5, 11.106601717798213, 12, 15.5),
-			Block.box(4.893398282201788, 1, 14.5, 11.106601717798213, 12, 15.5),
-			Block.box(0.5, 1, 4.893398282201788, 1.5, 12, 11.106601717798213),
-			Block.box(0.5, 1, 4.893398282201788, 1.5, 12, 11.106601717798213),
-			Block.box(14.5, 1, 4.893398282201788, 15.5, 12, 11.106601717798213),
-			Block.box(14.5, 1, 4.893398282201788, 15.5, 12, 11.106601717798213),
-			Block.box(4.686291501015241, 0, 0, 11.31370849898476, 1, 16),
-			Block.box(4.686291501015241, 0, 0, 11.31370849898476, 1, 16),
-			Block.box(0, 0, 4.686291501015241, 16, 1, 11.31370849898476),
-			Block.box(0, 0, 4.686291501015241, 16, 1, 11.31370849898476)
-	).reduce((v1, v2) -> {return VoxelShapes.join(v1, v2, IBooleanFunction.OR);}).get();
 
 	public HalfBarrelBlock(Properties properties) {
 		super(properties);
@@ -62,7 +40,7 @@ public class HalfBarrelBlock extends Block {
 
 	@Override
 	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-		return SHAPE;
+		return VoxelShapes.box(0.01d, 0.01d, 0.01d, 0.99d, 0.75d, 0.99d);
 	}
 
 
