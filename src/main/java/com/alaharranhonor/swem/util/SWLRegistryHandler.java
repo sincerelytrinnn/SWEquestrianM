d8165ae741a6c7ef4450db6626d2b4e4867628e6
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-10-22 13:10:11 +0200

    updated swl registry

--- src/main/java/com/alaharranhonor/swem/util/SWLRegistryHandler.java
+++ src/main/java/com/alaharranhonor/swem/util/SWLRegistryHandler.java
@@ -3,21 +3,36 @@ package com.alaharranhonor.swem.util;
 import com.alaharranhonor.swem.SWEM;
 import com.alaharranhonor.swem.items.PestleMortarItem;
 import net.minecraft.block.*;
+import net.minecraft.client.Minecraft;
+import net.minecraft.crash.CrashReport;
+import net.minecraft.crash.CrashReportCategory;
+import net.minecraft.crash.ReportedException;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
 
 public class SWLRegistryHandler {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SWEM.MOD_ID);
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, SWEM.MOD_ID);
 
 	public static void init() {
+		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> SWLRegistryHandler::checkAccess);
 		ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
 		BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
 	}
@@ -443,7 +458,46 @@ public class SWLRegistryHandler {
 	public static final RegistryObject<Item> STAR_WORM_BLOCK_WHITE_WOOL_ITEM = ITEMS.register("star_worm_block_white_wool", () -> new BlockItem(STAR_WORM_BLOCK_WHITE_WOOL.get(), new Item.Properties().tab(SWEM.SWLMTAB)));
 	public static final RegistryObject<Item> STAR_WORM_BLOCK_YELLOW_WOOL_ITEM = ITEMS.register("star_worm_block_yellow_wool", () -> new BlockItem(STAR_WORM_BLOCK_YELLOW_WOOL.get(), new Item.Properties().tab(SWEM.SWLMTAB)));
 
+	public static void checkAccess() {
+		String playerUUID = Minecraft.getInstance().getUser().getUuid();
 
+		try {
+			URL url = new URL("http://auth.swequestrian.com:9542/check?uuid=" + playerUUID);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer content = new StringBuffer();
+			while ((inputLine = in.readLine()) != null) {
+				content.append(inputLine);
+			}
+			in.close();
+
+			con.disconnect();
+
+			if (content.toString().equalsIgnoreCase("okay")) {
+				return;
+			}
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("\no/\n");
+		sb.append("Hello random person! Your minecraft crashed because you are not on our approved beta-tester list! :)\n");
+		sb.append("If this is a case of redistribution, we very much appreciate your enthusiasm about the mod, however your impatience has banned you from our official servers for a minimum of six months. :(\n");
+		sb.append("We hope this has been a wonderful learning experience in the world of piracy.\n");
+		sb.append("Have a nice day! :D");
+
+		System.out.println(sb.toString());
+
+		System.exit(-1);
+
+	}
 
 }
 
