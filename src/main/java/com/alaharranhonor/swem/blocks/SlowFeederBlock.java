f9152cfdf3ce7e14ab458888ad8f1611ddeeab07
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-10-22 12:29:40 +0200

    shrink the stack, when accepted

--- src/main/java/com/alaharranhonor/swem/blocks/SlowFeederBlock.java
+++ src/main/java/com/alaharranhonor/swem/blocks/SlowFeederBlock.java
@@ -70,6 +70,9 @@ public class SlowFeederBlock extends Block {
 			if (item == SWEMBlocks.QUALITY_BALE_ITEM.get() && level_vanilla == 0) {
 				if (level_swem == 0) {
 					this.setHayLevel(worldIn, pos, state, LEVEL, 2);
+					if (!player.isCreative()) {
+						itemstack.shrink(1);
+					}
 					return ActionResultType.sidedSuccess(worldIn.isClientSide);
 				} else {
 					return ActionResultType.PASS;
@@ -78,6 +81,9 @@ public class SlowFeederBlock extends Block {
 			} else if (item == Items.HAY_BLOCK && level_swem == 0) {
 				if (level_vanilla == 0) {
 					this.setHayLevel(worldIn, pos, state, LEVEL_VANILLA, 2);
+					if (!player.isCreative()) {
+						itemstack.shrink(1);
+					}
 					return ActionResultType.sidedSuccess(worldIn.isClientSide);
 				} else {
 					return ActionResultType.PASS;
