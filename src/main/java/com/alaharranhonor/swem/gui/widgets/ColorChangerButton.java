75355cd2b01f82101a8578630ea33c0e732a8dab
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-10-27 11:31:01 +0200

    fixed color changer button crash

--- src/main/java/com/alaharranhonor/swem/gui/widgets/ColorChangerButton.java
+++ src/main/java/com/alaharranhonor/swem/gui/widgets/ColorChangerButton.java
@@ -7,6 +7,8 @@ import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.item.DyeColor;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextFormatting;
 
 
 public class ColorChangerButton extends CycableButton {
@@ -30,6 +32,9 @@ public class ColorChangerButton extends CycableButton {
 
 	@Override
 	public ITextComponent getMessage() {
+		if (this.screen.layerAmount < this.layer) {
+			return new StringTextComponent("White");
+		}
 		String colorName = DyeColor.byId(this.screen.layerColors.get(this.layer)).getName();
 		String[] names = colorName.split("_");
 		String finalName = "";
