9c7e51efee84ab97cb41557de65d5f37f2c929b1
Silverwolf11420 <treefrog11420@gmail.com>
2022-01-20 05:28:49 -0600

    Adjusted compost

--- src/main/java/com/alaharranhonor/swem/util/registry/SWEMItems.java
+++ src/main/java/com/alaharranhonor/swem/util/registry/SWEMItems.java
@@ -151,7 +151,7 @@ public class SWEMItems {
 	public static final RegistryObject<Item> STAR_WORM = ITEMS.register("star_worm", ItemBase::new);
 	public static final RegistryObject<Item> STAR_WORM_GOOP = ITEMS.register("star_worm_goop", ItemBase::new);
 	public static final RegistryObject<Item> PESTLE_MORTAR = ITEMS.register("pestle_mortar", ItemBase::new);
-
+	public static final RegistryObject<Item> BONE_MEAL_COMPOST = ITEMS.register("bone_meal_compost", () -> new BoneMealItem(new Item.Properties().tab(SWEM.TAB)));
 
 
 	// Rainbow Horse recipe items.
