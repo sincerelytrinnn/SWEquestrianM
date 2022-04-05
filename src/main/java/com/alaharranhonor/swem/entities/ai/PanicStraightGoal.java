05d6742214e01093728026de6734ce4221e76cfd
Magnus Jensen <magnushjensen.mail@gmail.com>
2022-04-05 18:37:14 +0200

    adjust panic goal

--- src/main/java/com/alaharranhonor/swem/entities/ai/PanicStraightGoal.java
+++ src/main/java/com/alaharranhonor/swem/entities/ai/PanicStraightGoal.java
@@ -16,13 +16,25 @@ package com.alaharranhonor.swem.entities.ai;
  */
 
 import com.alaharranhonor.swem.entities.SWEMHorseEntityBase;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.ai.goal.PanicGoal;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3i;
+import net.minecraft.world.IBlockReader;
 
-public class PanicStraightGoal extends PanicGoal {
+import javax.annotation.Nullable;
+
+public class PanicStraightGoal extends Goal {
 	private final SWEMHorseEntityBase horse;
+	protected final double speedModifier;
+	protected double posX;
+	protected double posY;
+	protected double posZ;
+	protected boolean isRunning;
 
 	/**
 	 * Instantiates a new Panic straight goal.
@@ -31,29 +43,110 @@ public class PanicStraightGoal extends PanicGoal {
 	 * @param speedIn  the speed in
 	 */
 	public PanicStraightGoal(SWEMHorseEntityBase creature, double speedIn) {
-		super(creature, speedIn);
 		this.horse = creature;
+		this.speedModifier = speedIn;
 	}
 
 	@Override
 	public void start() {
-		super.start();
+		this.horse.getNavigation().stop();
+		SWEMHorseEntityBase.HorseSpeed oldSpeed = this.horse.currentSpeed;
+		this.horse.currentSpeed = SWEMHorseEntityBase.HorseSpeed.GALLOP;
+		this.horse.updateSelectedSpeed(oldSpeed);
+		this.horse.getNavigation().moveTo(this.posX, this.posY, this.posZ, this.speedModifier);
+		this.isRunning = true;
+	}
+
+	/**
+	 * Reset the task's internal state. Called when this task is interrupted by another one
+	 */
+	@Override
+	public void stop() {
 		SWEMHorseEntityBase.HorseSpeed oldSpeed = this.horse.currentSpeed;
 		this.horse.currentSpeed = SWEMHorseEntityBase.HorseSpeed.WALK;
 		this.horse.updateSelectedSpeed(oldSpeed);
+		this.horse.getNavigation().stop();
 	}
 
-	@Override
 	protected boolean findRandomPosition() {
-		Direction direction = this.mob.getDirection();
+		Direction direction = this.horse.getDirection();
 		Vector3i vector3i = direction.getNormal();
 		vector3i = vector3i.relative(direction, 5);
-		BlockPos currentPos = this.mob.blockPosition();
+		BlockPos currentPos = this.horse.blockPosition();
 		BlockPos pos = currentPos.offset(vector3i);
 		this.posX = pos.getX();
 		this.posY = pos.getY();
 		this.posZ = pos.getZ();
 		return true;
 
+	}
+
+	/**
+	 * Returns whether execution should begin. You can also read and cache any state necessary for execution in this
+	 * method as well.
+	 */
+	@Override
+	public boolean canUse() {
+		if (this.horse.getLastHurtByMob() == null && !this.horse.isOnFire() && !this.horse.isVehicle()) {
+			return false;
+		} else {
+			if (this.horse.isOnFire()) {
+				BlockPos blockpos = this.lookForWater(this.horse.level, this.horse, 5, 4);
+				if (blockpos != null) {
+					this.posX = (double)blockpos.getX();
+					this.posY = (double)blockpos.getY();
+					this.posZ = (double)blockpos.getZ();
+					return true;
+				}
+			}
+
+			return this.findRandomPosition();
+		}
+	}
+
+	/**
+	 * Returns whether an in-progress EntityAIBase should continue executing
+	 */
+	@Override
+	public boolean canContinueToUse() {
+		return !this.horse.getNavigation().isDone();
+	}
+
+	/**
+	 * Keep ticking a continuous task that has already been started
+	 */
+	@Override
+	public void tick() {
+		if (this.horse.getNavigation().isDone()) {
+			this.stop();
+		}
+	}
+
+	@Nullable
+	protected BlockPos lookForWater(IBlockReader pLevel, Entity pEntity, int pHorizontalRange, int pVerticalRange) {
+		BlockPos blockpos = pEntity.blockPosition();
+		int i = blockpos.getX();
+		int j = blockpos.getY();
+		int k = blockpos.getZ();
+		float f = (float)(pHorizontalRange * pHorizontalRange * pVerticalRange * 2);
+		BlockPos blockpos1 = null;
+		BlockPos.Mutable blockpos$mutable = new BlockPos.Mutable();
+
+		for(int l = i - pHorizontalRange; l <= i + pHorizontalRange; ++l) {
+			for(int i1 = j - pVerticalRange; i1 <= j + pVerticalRange; ++i1) {
+				for(int j1 = k - pHorizontalRange; j1 <= k + pHorizontalRange; ++j1) {
+					blockpos$mutable.set(l, i1, j1);
+					if (pLevel.getFluidState(blockpos$mutable).is(FluidTags.WATER)) {
+						float f1 = (float)((l - i) * (l - i) + (i1 - j) * (i1 - j) + (j1 - k) * (j1 - k));
+						if (f1 < f) {
+							f = f1;
+							blockpos1 = new BlockPos(blockpos$mutable);
+						}
+					}
+				}
+			}
+		}
+
+		return blockpos1;
 	}
 }
