dd6ccf8d89ef35f7e7b55bc52353131efd59851b
Magnus Jensen <magnushjensen.mail@gmail.com>
2022-04-08 19:35:04 +0200

    implemented mercy blade

--- src/main/java/com/alaharranhonor/swem/util/registry/SWEMItems.java
+++ src/main/java/com/alaharranhonor/swem/util/registry/SWEMItems.java
@@ -158,7 +158,7 @@ public class SWEMItems {
 	public static final RegistryObject<Item> PESTLE_MORTAR = ITEMS.register("pestle_mortar", () -> new PestleMortarItem(new Item.Properties().stacksTo(1).tab(SWEM.TAB)));
 	public static final RegistryObject<Item> BONE_MEAL_COMPOST = ITEMS.register("bone_meal_compost", () -> new BoneMealItem(new Item.Properties().tab(SWEM.TAB)));
 	public static final RegistryObject<Item> BRUSH = ITEMS.register("brush", BrushItem::new);
-	public static final RegistryObject<Item> MERCY_BLADE = ITEMS.register("mercy_blade", ItemBase::new);
+	public static final RegistryObject<Item> MERCY_BLADE = ITEMS.register("mercy_blade", MercyBlade::new);
 
 	// Rainbow Horse recipe items.
 
