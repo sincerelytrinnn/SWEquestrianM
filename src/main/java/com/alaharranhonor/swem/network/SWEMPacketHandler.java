eb69d4b2923dd328298a30c58c99a7382abb9c04
Magnus Jensen <magnushjensen.mail@gmail.com>
2022-06-21 21:37:26 +0200

    update version

--- src/main/java/com/alaharranhonor/swem/network/SWEMPacketHandler.java
+++ src/main/java/com/alaharranhonor/swem/network/SWEMPacketHandler.java
@@ -21,7 +21,7 @@ import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 public class SWEMPacketHandler {
-  private static final String PROTOCOL_VERSION = "3.1.0-b";
+  private static final String PROTOCOL_VERSION = "3.6.1-b";
   public static final SimpleChannel INSTANCE =
       NetworkRegistry.newSimpleChannel(
           new ResourceLocation(SWEM.MOD_ID, "main"),
