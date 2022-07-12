8a655bb4bf9ebb9b66e74f6914ca7344d2de56d5
Emily Canto <56757680+dronade@users.noreply.github.com>
2022-07-12 17:41:09 +0100

    added block of water base

--- src/main/java/com/alaharranhonor/swem/blocks/BlockOWater.java
+++ src/main/java/com/alaharranhonor/swem/blocks/BlockOWater.java
@@ -1,14 +1,23 @@
 package com.alaharranhonor.swem.blocks;
 
+import com.alaharranhonor.swem.util.registry.SWEMTileEntities;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.util.Direction;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IWorldReader;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
+
+import javax.annotation.Nullable;
+
 
 public class BlockOWater extends Block {
     /** Instantiates a Block O' Water. */
@@ -20,4 +29,23 @@ public class BlockOWater extends Block {
                         .harvestLevel(1)
                         .harvestTool(ToolType.PICKAXE));
     }
+
+    public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand) {
+        ItemStack itemHeld = player.getItemInHand(hand);
+        TileEntity tank = world.getBlockEntity(pos);
+        //do stuff here
+        return null;
+    }
+
+    @Override
+    public boolean hasTileEntity(BlockState state) {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+        return SWEMTileEntities.BLOCK_O_WATER_TILE_ENTITY.get().create();
+    }
+
 }
---
+++ src/main/java/com/alaharranhonor/swem/tileentity/BlockOWaterTE.java
@@ -0,0 +1,12 @@
+package com.alaharranhonor.swem.tileentity;
+
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
+
+public class BlockOWaterTE extends TileEntity {
+
+
+    public BlockOWaterTE(TileEntityType<?> p_i48289_1_) {
+        super(p_i48289_1_);
+    }
+}
