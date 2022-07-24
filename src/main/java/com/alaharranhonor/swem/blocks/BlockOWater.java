98dd145353476d966c817e7b705b295ecdb81062
Emily Canto <56757680+dronade@users.noreply.github.com>
2022-07-24 23:43:24 +0100

    added block o' water functionality + recipe

--- src/main/java/com/alaharranhonor/swem/blocks/BlockOWater.java
+++ src/main/java/com/alaharranhonor/swem/blocks/BlockOWater.java
@@ -6,12 +6,17 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
-
 
 public class BlockOWater extends Block {
     /**
@@ -26,9 +31,33 @@ public class BlockOWater extends Block {
                         .harvestTool(ToolType.PICKAXE));
     }
 
-    //re-doing it because previous implementation was overkill
-    public boolean onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand) {
-        ItemStack itemInHand = player.getItemInHand(hand);
-        return true;
+    /**
+     * Fills buckets with water.
+     */
+    @Override
+    public ActionResultType use(
+            BlockState state,
+            World worldIn,
+            BlockPos pos,
+            PlayerEntity player,
+            Hand handIn,
+            BlockRayTraceResult p_225533_6_) {
+        ItemStack itemstack = player.getItemInHand(handIn);
+        Item item = itemstack.getItem();
+        if (itemstack.isEmpty()) {
+            return ActionResultType.PASS;
+        } else if (item == Items.BUCKET) {
+            itemstack.shrink(1);
+            if (itemstack.isEmpty()) {
+                player.setItemInHand(handIn, new ItemStack(Items.WATER_BUCKET));
+            } else if (!player.inventory.add(new ItemStack(Items.WATER_BUCKET))) {
+                player.drop(new ItemStack(Items.WATER_BUCKET), false);
+            }
+            worldIn.playSound((PlayerEntity) null, pos, SoundEvents.BUCKET_FILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            return ActionResultType.sidedSuccess(worldIn.isClientSide);
+        } else {
+            return ActionResultType.PASS;
+        }
+
     }
 }
