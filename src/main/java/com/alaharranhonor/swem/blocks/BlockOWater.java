51d82cbb6701be4f8f5c9e6fee441f437cd8e96e
Magnus Jensen <magnushjensen.mail@gmail.com>
2022-08-08 19:25:03 +0200

    Updated BoW props

--- src/main/java/com/alaharranhonor/swem/blocks/BlockOWater.java
+++ src/main/java/com/alaharranhonor/swem/blocks/BlockOWater.java
@@ -2,9 +2,7 @@ package com.alaharranhonor.swem.blocks;
 
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
-import net.minecraftforge.common.ToolType;
+import net.minecraft.block.Blocks;
 
 public class BlockOWater extends Block {
     /**
@@ -12,10 +10,6 @@ public class BlockOWater extends Block {
      */
     public BlockOWater() {
         super(
-                AbstractBlock.Properties.of(Material.SPONGE)
-                        .strength(2.0f, 6.0f)
-                        .sound(SoundType.WET_GRASS)
-                        .harvestLevel(1)
-                        .harvestTool(ToolType.PICKAXE));
+            AbstractBlock.Properties.copy(Blocks.STONE));
     }
 }
