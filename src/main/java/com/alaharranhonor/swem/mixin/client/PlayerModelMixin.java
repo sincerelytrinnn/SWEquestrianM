4be7e37bf2402de64cf45f08aceb9a753a022fe2
Magnus Jensen <magnushjensen.mail@gmail.com>
2022-08-16 21:11:42 +0200

    Fix first-person hand animating

---
+++ src/main/java/com/alaharranhonor/swem/mixin/client/PlayerModelMixin.java
@@ -0,0 +1,78 @@
+
+/*
+ * All Rights Reserved
+ *
+ * Copyright (c) 2022, AlaharranHonor, Legenden.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.alaharranhonor.swem.mixin.client;
+
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.renderer.entity.model.PlayerModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerModel.class)
+public class PlayerModelMixin<T extends LivingEntity> extends BipedModel<T> {
+
+    @Shadow
+    public ModelRenderer leftSleeve;
+    @Shadow
+    public ModelRenderer rightSleeve;
+    @Shadow
+    public ModelRenderer leftPants;
+    @Shadow
+    public ModelRenderer rightPants;
+    @Shadow
+    public ModelRenderer jacket;
+    @Final
+    @Shadow
+    public ModelRenderer cloak;
+
+    public PlayerModelMixin(float p_i1148_1_) {
+        super(p_i1148_1_);
+    }
+
+    @Inject(method = "setupAnim(Lnet/minecraft/entity/LivingEntity;FFFFF)V", at = @At("HEAD"), cancellable = true)
+    public void setupAnim(T pEntity, float pLimbSwing, float pLimbSwingAmount, float pAgeInTicks, float pNetHeadYaw, float pHeadPitch, CallbackInfo ci) {
+        super.setupAnim(pEntity, pLimbSwing, pLimbSwingAmount, pAgeInTicks, pNetHeadYaw, pHeadPitch);
+        this.leftPants.copyFrom(this.leftLeg);
+        this.rightPants.copyFrom(this.rightLeg);
+        this.leftSleeve.copyFrom(this.leftArm);
+        this.rightSleeve.copyFrom(this.rightArm);
+        this.jacket.copyFrom(this.body);
+        if (pEntity.getItemBySlot(EquipmentSlotType.CHEST).isEmpty()) {
+            if (pEntity.isCrouching()) {
+                this.cloak.z = 1.4F;
+                this.cloak.y = 1.85F;
+            } else {
+                this.cloak.z = 0.0F;
+                this.cloak.y = 0.0F;
+            }
+        } else if (pEntity.isCrouching()) {
+            this.cloak.z = 0.3F;
+            this.cloak.y = 0.8F;
+        } else {
+            this.cloak.z = -1.1F;
+            this.cloak.y = -0.85F;
+        }
+
+        ci.cancel();
+
+    }
+}
