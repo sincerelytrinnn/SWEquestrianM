b7a4df26d1ab08359a95d1a596f66422aaed2173
Magnus Jensen <magnushjensen.mail@gmail.com>
2022-09-24 16:09:20 +0200

    fix(measurement-tool): Producing wrongly faced jumps

--- src/main/java/com/alaharranhonor/swem/tools/MeasurementTool.java
+++ src/main/java/com/alaharranhonor/swem/tools/MeasurementTool.java
@@ -35,6 +35,7 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.fml.network.NetworkHooks;
 
@@ -64,28 +65,20 @@ public class MeasurementTool extends Item {
             if (te instanceof JumpPasserTE) {
                 JumpPasserTE jumpPasser = (JumpPasserTE) te;
                 if (jumpPasser.getControllerPos() != null) {
-                    JumpTE controller =
-                            (JumpTE) context.getLevel().getBlockEntity(jumpPasser.getControllerPos());
-                    INamedContainerProvider provider =
-                            new INamedContainerProvider() {
-                                @Override
-                                public ITextComponent getDisplayName() {
-                                    return new TranslationTextComponent("screen.swem.jump_builder");
-                                }
+                    JumpTE controller = (JumpTE) context.getLevel().getBlockEntity(jumpPasser.getControllerPos());
+                    INamedContainerProvider provider = new INamedContainerProvider() {
+                        @Override
+                        public ITextComponent getDisplayName() {
+                            return new TranslationTextComponent("screen.swem.jump_builder");
+                        }
 
-                                @Nullable
-                                @Override
-                                public Container createMenu(
-                                        int p_createMenu_1_,
-                                        PlayerInventory p_createMenu_2_,
-                                        PlayerEntity p_createMenu_3_) {
-                                    return new JumpContainer(p_createMenu_1_, p_createMenu_2_, controller);
-                                }
-                            };
-                    NetworkHooks.openGui(
-                            (ServerPlayerEntity) context.getPlayer(),
-                            provider,
-                            buffer -> buffer.writeBlockPos(controller.getBlockPos()));
+                        @Nullable
+                        @Override
+                        public Container createMenu(int p_createMenu_1_, PlayerInventory p_createMenu_2_, PlayerEntity p_createMenu_3_) {
+                            return new JumpContainer(p_createMenu_1_, p_createMenu_2_, controller);
+                        }
+                    };
+                    NetworkHooks.openGui((ServerPlayerEntity) context.getPlayer(), provider, buffer -> buffer.writeBlockPos(controller.getBlockPos()));
                     // SWEMPacketHandler.INSTANCE.send(PacketDistributor.PLAYER.with(() ->
                     // (ServerPlayerEntity) context.getPlayer()), new OpenGuiPacket(controller.getPos(),
                     // controller.getLayerAmount(), controller.getCurrentStandard()));
@@ -95,26 +88,19 @@ public class MeasurementTool extends Item {
             if (te instanceof JumpTE) {
                 JumpTE jumpController = (JumpTE) te;
                 if (jumpController.layerAmount == 0) return ActionResultType.PASS;
-                INamedContainerProvider provider =
-                        new INamedContainerProvider() {
-                            @Override
-                            public ITextComponent getDisplayName() {
-                                return new TranslationTextComponent("screen.swem.jump_builder");
-                            }
+                INamedContainerProvider provider = new INamedContainerProvider() {
+                    @Override
+                    public ITextComponent getDisplayName() {
+                        return new TranslationTextComponent("screen.swem.jump_builder");
+                    }
 
-                            @Nullable
-                            @Override
-                            public Container createMenu(
-                                    int p_createMenu_1_,
-                                    PlayerInventory p_createMenu_2_,
-                                    PlayerEntity p_createMenu_3_) {
-                                return new JumpContainer(p_createMenu_1_, p_createMenu_2_, jumpController);
-                            }
-                        };
-                NetworkHooks.openGui(
-                        (ServerPlayerEntity) context.getPlayer(),
-                        provider,
-                        buffer -> buffer.writeBlockPos(jumpController.getBlockPos()));
+                    @Nullable
+                    @Override
+                    public Container createMenu(int p_createMenu_1_, PlayerInventory p_createMenu_2_, PlayerEntity p_createMenu_3_) {
+                        return new JumpContainer(p_createMenu_1_, p_createMenu_2_, jumpController);
+                    }
+                };
+                NetworkHooks.openGui((ServerPlayerEntity) context.getPlayer(), provider, buffer -> buffer.writeBlockPos(jumpController.getBlockPos()));
                 // SWEMPacketHandler.INSTANCE.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity)
                 // context.getPlayer()), new OpenGuiPacket(jumpController.getPos(),
                 // jumpController.getLayerAmount(), jumpController.getCurrentStandard()));
@@ -129,45 +115,41 @@ public class MeasurementTool extends Item {
             int[] posCords = nbt.getIntArray("pos1");
             BlockPos firstPos = new BlockPos(posCords[0], posCords[1], posCords[2]);
             BlockPos pos = context.getClickedPos();
-            int direction = isValidPos(firstPos, pos);
+            Direction initialDirection = Direction.byName(nbt.getString("direction"));
+            if (initialDirection != context.getHorizontalDirection()) {
+                nbt.remove("pos1");
+                nbt.remove("direction");
+                stack.setTag(nbt);
+                context.getPlayer().displayClientMessage(new StringTextComponent("You are not facing the same direction as the first position"), true);
+                return ActionResultType.FAIL;
+            }
+            int direction = isValidPos(firstPos, pos, initialDirection);
             if (direction == -1) {
                 nbt.remove("pos1");
+                nbt.remove("direction");
                 stack.setTag(nbt);
+                context.getPlayer().displayClientMessage(new StringTextComponent("The block gap should be on the other axis as you are facing."), true);
                 return ActionResultType.CONSUME;
             }
 
             ArrayList<BlockPos> blockPositions = new ArrayList<>();
-            BlockPos.betweenClosed(firstPos, pos)
-                    .forEach(
-                            (ps) -> {
-                                BlockPos pos1 = ps.immutable();
-                                blockPositions.add(pos1);
-                            });
+            BlockPos.betweenClosed(firstPos, pos).forEach((ps) -> {
+                BlockPos pos1 = ps.immutable();
+                blockPositions.add(pos1);
+            });
             int layerAmount = blockPositions.size() / 7;
             nbt.remove("pos1");
+            nbt.remove("direction");
             int lowestYValue = Math.min(firstPos.getY(), pos.getY());
             Map<Integer, ArrayList<BlockPos>> layers = this.rearrangeLayers(lowestYValue, blockPositions);
 
             // Should default to either the player facing north/east
 
-            Direction facing =
-                    context.getHorizontalDirection().getAxis() == Direction.Axis.Z
-                            ? Direction.SOUTH
-                            : Direction.WEST;
+            Direction facing = context.getHorizontalDirection().getAxis() == Direction.Axis.Z ? Direction.SOUTH : Direction.WEST;
 
-            context
-                    .getLevel()
-                    .setBlock(
-                            layers.get(1).get(0).relative(Direction.UP, 5),
-                            SWEMBlocks.JUMP_CONTROLLER
-                                    .get()
-                                    .defaultBlockState()
-                                    .setValue(JumpControllerBlock.FACING, facing),
-                            3);
+            context.getLevel().setBlock(layers.get(1).get(0).relative(Direction.UP, 5), SWEMBlocks.JUMP_CONTROLLER.get().defaultBlockState().setValue(JumpControllerBlock.FACING, facing), 3);
 
-            JumpTE jumpController =
-                    (JumpTE)
-                            context.getLevel().getBlockEntity(layers.get(1).get(0).relative(Direction.UP, 5));
+            JumpTE jumpController = (JumpTE) context.getLevel().getBlockEntity(layers.get(1).get(0).relative(Direction.UP, 5));
             jumpController.setLayerAmount(layerAmount);
             jumpController.assignJumpBlocks(layers);
             jumpController.initStandards(StandardLayer.SCHOOLING);
@@ -175,26 +157,19 @@ public class MeasurementTool extends Item {
                 jumpController.placeLayer(i, JumpLayer.NONE);
             }
 
-            INamedContainerProvider provider =
-                    new INamedContainerProvider() {
-                        @Override
-                        public ITextComponent getDisplayName() {
-                            return new TranslationTextComponent("screen.swem.jump_builder");
-                        }
+            INamedContainerProvider provider = new INamedContainerProvider() {
+                @Override
+                public ITextComponent getDisplayName() {
+                    return new TranslationTextComponent("screen.swem.jump_builder");
+                }
 
-                        @Nullable
-                        @Override
-                        public Container createMenu(
-                                int p_createMenu_1_,
-                                PlayerInventory p_createMenu_2_,
-                                PlayerEntity p_createMenu_3_) {
-                            return new JumpContainer(p_createMenu_1_, p_createMenu_2_, jumpController);
-                        }
-                    };
-            NetworkHooks.openGui(
-                    (ServerPlayerEntity) context.getPlayer(),
-                    provider,
-                    buffer -> buffer.writeBlockPos(jumpController.getBlockPos()));
+                @Nullable
+                @Override
+                public Container createMenu(int p_createMenu_1_, PlayerInventory p_createMenu_2_, PlayerEntity p_createMenu_3_) {
+                    return new JumpContainer(p_createMenu_1_, p_createMenu_2_, jumpController);
+                }
+            };
+            NetworkHooks.openGui((ServerPlayerEntity) context.getPlayer(), provider, buffer -> buffer.writeBlockPos(jumpController.getBlockPos()));
 
             // SWEMPacketHandler.INSTANCE.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity)
             // context.getPlayer()), new OpenGuiPacket(jumpController.getPos(),
@@ -206,6 +181,7 @@ public class MeasurementTool extends Item {
         } else {
             BlockPos pos = context.getClickedPos();
             nbt.putIntArray("pos1", new int[]{pos.getX(), pos.getY(), pos.getZ()});
+            nbt.putString("direction", context.getHorizontalDirection().name());
             stack.setTag(nbt);
             return ActionResultType.CONSUME;
         }
@@ -218,8 +194,7 @@ public class MeasurementTool extends Item {
      * @param positions    the positions
      * @return the map
      */
-    private Map<Integer, ArrayList<BlockPos>> rearrangeLayers(
-            int lowestYValue, ArrayList<BlockPos> positions) {
+    private Map<Integer, ArrayList<BlockPos>> rearrangeLayers(int lowestYValue, ArrayList<BlockPos> positions) {
         Map<Integer, ArrayList<BlockPos>> layers = new HashMap<>();
         for (int i = 1; i <= positions.size(); i++) {
             BlockPos pos = positions.get(i - 1);
@@ -239,19 +214,29 @@ public class MeasurementTool extends Item {
      * @param second the second
      * @return the int
      */
-    private int isValidPos(BlockPos first, BlockPos second) {
+    private int isValidPos(BlockPos first, BlockPos second, Direction initialDirection) {
         if (first.getY() != second.getY()) {
             return -1;
         }
 
-        if (first.getX() == second.getX()) {
+        if (initialDirection.getAxis() == Direction.Axis.Z) {
+            if (first.getZ() != second.getZ()) {
+                return -1;
+            }
+        } else {
+            if (first.getX() != second.getX()) {
+                return -1;
+            }
+        }
+
+        if (initialDirection.getAxis() == Direction.Axis.X) {
             if (first.getZ() - second.getZ() == -6) {
                 return 1; // Negative Z
             } else if (first.getZ() - second.getZ() == 6) {
                 return 2; // Positive Z
             }
             // check < or > 5 on z
-        } else if (first.getZ() == second.getZ()) {
+        } else if (initialDirection.getAxis() == Direction.Axis.Z) {
             if (first.getX() - second.getX() == -6) {
                 return 3; // Negative X
             } else if (first.getX() - second.getX() == 6) {
